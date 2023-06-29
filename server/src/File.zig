3915dcf59650ca89989b95b8ffde1c84818d5949
Marcus Wågberg <marcuswagberg@protonmail.com>
2023-06-29 10:17:48 +0200

    Add ftype field to File.V1

--- server/src/File.zig
+++ server/src/File.zig
@@ -5,16 +5,19 @@ pub const V1 = struct {
     const Self = @This();
 
     name: []const u8,
+    ftype: []const u8,
     group: [36]u8,
     uploaded: i128,
 
     pub fn free(self: *const Self, alloc: Allocator) void {
         alloc.free(self.name);
+        alloc.free(self.ftype);
     }
 
     pub fn copy(self: *const Self, alloc: Allocator) ?Self {
         return Self{
             .name = alloc.dupe(u8, self.name) catch return null,
+            .ftype = alloc.dupe(u8, self.ftype) catch return null,
             .group = self.group,
             .uploaded = self.uploaded,
         };
