188d052bb552daf7164ac67f328793b8bb66442e
Marcus Wågberg <marcuswagberg@protonmail.com>
2023-09-11 20:36:21 +0200

    Update to zig 0.11.0 and clean up build system

--- server/build.zig
+++ server/build.zig
@@ -2,25 +2,18 @@ const std = @import("std");
 
 const Self = @This();
 
-var zap: ?*std.Build.Dependency = null;
-var clap: ?*std.Build.Dependency = null;
-
 exe: *std.Build.Step.Compile,
 run: *std.Build.Step.Run,
 
 pub fn add(b: *std.Build, comptime server_root: []const u8, target: std.zig.CrossTarget, optimize: std.builtin.Mode) Self {
-    if (zap == null) {
-        zap = b.dependency("zap", .{
-            .target = target,
-            .optimize = optimize,
-        });
-    }
-    if (clap == null) {
-        clap = b.dependency("clap", .{
-            .target = target,
-            .optimize = optimize,
-        });
-    }
+    const zap = b.dependency("zap", .{
+        .target = target,
+        .optimize = optimize,
+    });
+    const clap = b.dependency("clap", .{
+        .target = target,
+        .optimize = optimize,
+    });
 
     const exe = b.addExecutable(.{
         .name = "mercury-server",
@@ -28,9 +21,9 @@ pub fn add(b: *std.Build, comptime server_root: []const u8, target: std.zig.Cros
         .target = target,
         .optimize = optimize,
     });
-    exe.addModule("zap", zap.?.module("zap"));
-    exe.linkLibrary(zap.?.artifact("facil.io"));
-    exe.addModule("clap", clap.?.module("clap"));
+    exe.addModule("zap", zap.module("zap"));
+    exe.linkLibrary(zap.artifact("facil.io"));
+    exe.addModule("clap", clap.module("clap"));
 
     const run = b.addRunArtifact(exe);
     if (b.args) |args| {
